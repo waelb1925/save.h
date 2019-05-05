645c5d2e5bf11cace80342596f92ce8ab26f0c61
waelb1925 <47981286+waelb1925@users.noreply.github.com>
2019-05-05 07:58:04 -0700

    Add files via upload

---
+++ save.h
@@ -0,0 +1,10 @@
+#ifndef SAVE_INCLUDED
+#define SAVE_INCLUDED
+
+#include "jeu.h" 
+#include "voiture.h"
+#include"background.h"
+
+void sauvegarde (char fich[40],jeu *game);
+void lire (char fich[40],jeu *game);
+#endif // SAVE_INCLUDED
